927973e3bb55858e69dbe5b873a91523cb415716
Binit Chandra Jha <030binit@gmail.com>
2026-04-17 23:44:06 +0545

    splash-screen added

--- app/src/main/java/com/example/uptodo/MainActivity.kt
+++ app/src/main/java/com/example/uptodo/MainActivity.kt
@@ -12,10 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uptodo.ui.theme.UpTodoTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         enableEdgeToEdge()
         setContent {
             UpTodoTheme {
