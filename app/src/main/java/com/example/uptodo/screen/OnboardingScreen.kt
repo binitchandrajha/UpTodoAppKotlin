59f8f05365c99bf39c91d3a3fb65a8e6b427296c
Binit Chandra Jha <030binit@gmail.com>
2026-04-22 22:46:31 +0545

    skip button ui integration

--- app/src/main/java/com/example/uptodo/screen/OnboardingScreen.kt
+++ app/src/main/java/com/example/uptodo/screen/OnboardingScreen.kt
@@ -1,9 +1,36 @@
 package com.example.uptodo.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun OnboardingScreen(onFinish: () -> Unit){
-    Text(text = "Onboarding screen")
-}
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color(0xFF121212)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            TextButton(
+                onClick = {},
+                modifier = Modifier.padding(16.dp)
+            ) {
+               Text(
+                   text = "SKIP",
+                   color = Color.White.copy(alpha = 0.6f)
+               )
+            }
+        }
+    }
+    }
